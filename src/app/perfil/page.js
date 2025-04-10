dffac0011c7efae43667f9897ac0c081a13e1328
palazonfotos <morer.vajav22@zaragoza.salesianos.edu>
2025-04-10 19:43:32 +0200

    Add profile

---
+++ src/app/perfil/page.js
@@ -0,0 +1,17 @@
+import MainButton from "../components/buttons/mainButton";
+
+export default function Perfil() {
+
+    let nombre = "Juan";
+    let edad = 25;
+    edad += 50;
+
+    return (
+        <>
+            <div>Nombre: {nombre}</div>
+            <div>Edad: {edad}</div>
+            <MainButton>Hola</MainButton>
+            <MainButton>Adios</MainButton>
+        </>
+    );
+}
